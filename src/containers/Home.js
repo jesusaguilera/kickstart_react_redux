e8d337de33f5d3e12891bd29b09415b6325b8ac8
jesusaguilera <jesusaguilera09@gmail.com>
2020-10-09 22:32:40 +0200

    added useDispatch

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,7 +1,7 @@
 // Dependencies
-import React from "react";
+import React, { useState } from "react";
 import PropTypes from "prop-types";
-import { connect } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 
 // Actions
 import { addProject } from "../store/actions/projects";
@@ -9,34 +9,53 @@ import { addProject } from "../store/actions/projects";
 //Components
 import Header from "../components/Header";
 
-const renderProjects = (projects) => {
-  return projects.map((project, index) => {
-    return (
-      <div className="c-projects__wrapper" key={index}>
-        <p>{project.title}</p>
-        <p>{project.description}</p>
-      </div>
-    );
-  });
-};
-
-const handleAddProject = (props) => {
-  props.addProject({
-    title: "Second project title",
-    description: "Second project description",
-  });
-};
 
 // Component
 const Home = (props) => {
-  console.log(props.projects);
+
+  // useState
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+
+  // useDispatch
+  const dispatch = useDispatch();
+  const addProjectDispatch = (project) => dispatch(addProject(project));
+
+  // useSelector
+  const projects = useSelector(state => state.projects.projects); 
+
+  // Render projects 
+  const renderProjects = (projects) => {
+    return projects.map((project, index) => {
+      return (
+        <div className="c-projects__wrapper" key={index}>
+          <p>{project.title}</p>
+          <p>{project.description}</p>
+        </div>
+      );
+    });
+  };
+
+  // Handle submit
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    addProjectDispatch({
+      title,
+      description,
+    });
+  };
+
   return (
     <main>
       <Header />
       <div>
         <h1>Home</h1>
-        <button onClick={() => handleAddProject(props)}>Add project</button>
-        {renderProjects(props.projects)}
+        <form onSubmit={handleSubmit}>
+          <input value={title} onChange={e => setTitle(e.currentTarget.value)} />
+          <textArea value={title} onChange={e => setDescription(e.currentTarget.value)} />
+          <button type="submit">Add project</button>
+        </form>
+        {renderProjects(projects)}
       </div>
     </main>
   );
@@ -46,14 +65,15 @@ const Home = (props) => {
 Home.propTypes = {};
 
 // Redux
-const mapStateToProps = (state) => {
-  return {
-    projects: state.projects.projects,
-  };
-};
+// const mapStateToProps = (state) => {
+//   return {
+//     projects: state.projects.projects,
+//   };
+// };
 
-const mapDispatchToProps = {
-  addProject,
-};
+// const mapDispatchToProps = {
+//   addProject,
+// };
 
-export default connect(mapStateToProps, mapDispatchToProps)(Home);
+// export default connect(mapStateToProps, mapDispatchToProps)(Home);
+export default Home;
